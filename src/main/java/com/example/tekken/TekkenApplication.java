edde119dc76c0a62dc5a66f34511f448a71d82f1
shekdns <aah03@naver.com>
2026-05-19 21:28:32 +0900

    초기셋팅

---
+++ src/main/java/com/example/tekken/TekkenApplication.java
@@ -0,0 +1,12 @@
+package com.example.tekken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekkenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TekkenApplication.class, args);
+    }
+}
